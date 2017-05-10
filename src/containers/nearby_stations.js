5ca77b8fea6367928d8d8b4136a301c1dcbe3e49
Carl Tablante <carl@guavapass.com>
2017-05-10 20:24:08 +0800

    basic setup for react

---
+++ src/containers/nearby_stations.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NearbyStations extends Component {
+  render() {
+    return(
+      <div>
+        Hello World
+      </div>
+    );
+  }
+}
+
+export default NearbyStations;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+//import { Provider } from 'react-redux';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+
+import NearbyStations from './containers/nearby_stations';
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <div>
+      <Route exact path="/" component={NearbyStations} />
+    </div>
+  </Router>
+  ,
   document.getElementById('root')
 );
